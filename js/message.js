851dc0bb6cdc1f2ff99e2adde254cf0bef3dc902
zhuanghaixin <649308293@qq.com>
2018-06-20 16:01:15 +0800

    实现留言板 MVC中的V C

--- js/message.js
+++ js/message.js
@@ -1,71 +1,91 @@
-var APP_ID = 'mhcytAQxMeeBSOjUvwK44nE1-gzGzoHsz';
-var APP_KEY = 'oqRE4rqhtKKyuI0HGntyCFgV';
+!function () {
+    var view = document.querySelector('#siteMessage');
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
+    var controller = {
+        view: null,
+        messageList: null,
+        postMessageForm: null,
+        init: function (view) {
+            this.view = view;
+            this.messageList = view.querySelector('.messageList');
+            this.postMessageForm = view.querySelector('.postMessageForm');
+            this.initAV();
+            this.loadMessage();
+            this.bindEvents();
+        },
+        initAV: function () {
+            var APP_ID = 'mhcytAQxMeeBSOjUvwK44nE1-gzGzoHsz';
+            var APP_KEY = 'oqRE4rqhtKKyuI0HGntyCFgV';
+            AV.init({appId: APP_ID, appKey: APP_KEY});
+        },
+        loadMessage: function () {
+            var query = new AV.Query('Message');
+            query.find()
+                .then((
+                    messages) => {
+                    let messageArray = messages.map((item) => item.attributes);
 
+                    messageArray.forEach((item) => {
+                        let li = document.createElement('li');
+                        let content = item.content;
+                        let name = item.name;
+                        li.innerText = `${name}:${content}`;
+                        this.messageList.appendChild(li);
+                    })
+                }).then(function (messages) {
+                // 更新成功
 
-let postMessageForm = document.querySelector('.postMessageForm');
-console.log(postMessageForm);
-postMessageForm.addEventListener('submit', function (e) {
-    e.preventDefault();//阻止默认事件
-    let content = postMessageForm.querySelector('input[name=content').value;
-    let name = postMessageForm.querySelector('input[name=name').value;
-    // 创建TestObject表
-    var Message = AV.Object.extend('Message');
-// 在表中创建一行数据
-    var message = new Message();
-// 数据内容是 wordsL'Hello Word' 保存
-// 如果保存成功则运行 alert(' ');
-    message.save({
-        'content': content,
-        'name':name
-    }).then(function (object) {
-        // alert('数据提交成功');
-        // window.location.reload();  有刷新留言
-        let li=document.createElement('li');
-        let content=object.attributes.content;
-        let name=object.attributes.content;
-        let messageList=document.querySelector('.messageList');
-        li.innerText=`${name}:${content}`;
-        messageList.appendChild(li);
-    },function (error) {
-        alert('请改天留言');
-    })
-})
+            }, function (error) {
+                // 异常处理
+                console.log(error);
+            });
+        },
+        bindEvents: function () {
 
+            this.postMessageForm.addEventListener('submit', (e) =>{
+                e.preventDefault();//阻止默认事件
+                this.saveMessage();
+            })
 
-var query = new AV.Query('Message');
-query.find()
-    .then(function (messages) {
-    let messageArray=messages.map((item)=>item.attributes);
-        let messageList=document.querySelector('.messageList');
-    messageArray.forEach((item)=>{
-        let li=document.createElement('li');
-        let content=item.content;
-        let name=item.name;
-        li.innerText=`${name}:${content}`;
-        messageList.appendChild(li);
-    })
-}).then(function (messages) {
-    // 更新成功
+            // // 创建TestObject表
+            // var Test= AV.Object.extend('Test');
+            // // 在表中创建一行数据
+            // var test = new Test();
+            // // 数据内容是 wordsL'Hello Word' 保存
+            // // 如果保存成功则运行 alert(' ');
+            // test.save({
+            //     content: 'hhhh'
+            // }).then(function(object) {
+            //     alert('OK');
+            // })
+        },
+        saveMessage: function () {
+            var postMessageForm = this.postMessageForm;
+            let content = postMessageForm.querySelector('input[name=content').value;
+            let name = postMessageForm.querySelector('input[name=name').value;
+            // 创建TestObject表
+            var Message = AV.Object.extend('Message');
+            // 在表中创建一行数据
+            var message = new Message();
+            // 数据内容是 wordsL'Hello Word' 保存
+            // 如果保存成功则运行 alert(' ');
+            message.save({
+                'content': content,
+                'name': name
+            }).then(function (object) {
+                // alert('数据提交成功');
+                // window.location.reload();  有刷新留言
+                let li = document.createElement('li');
+                let content = object.attributes.content;
+                let name = object.attributes.content;
+                let messageList = document.querySelector('.messageList');
+                li.innerText = `${name}:${content}`;
+                messageList.appendChild(li);
+            }, function (error) {
+                alert('请改天留言');
+            })
+        }
+    }
+    controller.init.call(controller, view);
 
-}, function (error) {
-    // 异常处理
-    console.log(error);
-});
-
-
-// // 创建TestObject表
-// var Test= AV.Object.extend('Test');
-// // 在表中创建一行数据
-// var test = new Test();
-// // 数据内容是 wordsL'Hello Word' 保存
-// // 如果保存成功则运行 alert(' ');
-// test.save({
-//     content: 'hhhh'
-// }).then(function(object) {
-//     alert('OK');
-// })
+}.call();
