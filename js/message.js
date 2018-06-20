e09996de7878213628352c3d56b536165c93492b
zhuanghaixin <649308293@qq.com>
2018-06-20 16:57:38 +0800

    留言板的Model

--- js/message.js
+++ js/message.js
@@ -1,26 +1,49 @@
 !function () {
     var view = document.querySelector('#siteMessage');
-
-    var controller = {
-        view: null,
-        messageList: null,
-        postMessageForm: null,
-        init: function (view) {
-            this.view = view;
-            this.messageList = view.querySelector('.messageList');
-            this.postMessageForm = view.querySelector('.postMessageForm');
-            this.initAV();
-            this.loadMessage();
-            this.bindEvents();
-        },
-        initAV: function () {
+    var model={
+        init: function () {
             var APP_ID = 'mhcytAQxMeeBSOjUvwK44nE1-gzGzoHsz';
             var APP_KEY = 'oqRE4rqhtKKyuI0HGntyCFgV';
             AV.init({appId: APP_ID, appKey: APP_KEY});
         },
+        //获取数据
+        fetch:function () {
+            var query = new AV.Query('Message');
+            return query.find()  //Promise对象
+
+        },
+        //创建数据
+        save:function (name,content) {
+            // 创建TestObject表
+            var Message = AV.Object.extend('Message');
+            // 在表中创建一行数据
+            var message = new Message();
+            // 数据内容是 wordsL'Hello Word' 保存
+            // 如果保存成功则运行 alert(' ');
+            return message.save({
+                'content': content,
+                'name': name
+            })  //Promise对象
+        }
+    }
+    var controller = {
+        view: null,
+        model:null,
+        messageList: null,
+        postMessageForm: null,
+        init: function (view,model) {
+            this.view = view;
+            this.model=model;
+            this.messageList = view.querySelector('.messageList');
+            this.postMessageForm = view.querySelector('.postMessageForm');
+            this.model.init();
+            this.loadMessage();
+            this.bindEvents();
+        },
+
         loadMessage: function () {
             var query = new AV.Query('Message');
-            query.find()
+            this.model.fetch()
                 .then((
                     messages) => {
                     let messageArray = messages.map((item) => item.attributes);
@@ -63,29 +86,21 @@
             var postMessageForm = this.postMessageForm;
             let content = postMessageForm.querySelector('input[name=content').value;
             let name = postMessageForm.querySelector('input[name=name').value;
-            // 创建TestObject表
-            var Message = AV.Object.extend('Message');
-            // 在表中创建一行数据
-            var message = new Message();
-            // 数据内容是 wordsL'Hello Word' 保存
-            // 如果保存成功则运行 alert(' ');
-            message.save({
-                'content': content,
-                'name': name
-            }).then(function (object) {
+            this.model.save(name,content).then(function (object) {
                 // alert('数据提交成功');
                 // window.location.reload();  有刷新留言
                 let li = document.createElement('li');
                 let content = object.attributes.content;
-                let name = object.attributes.content;
+                let name = object.attributes.name;
                 let messageList = document.querySelector('.messageList');
                 li.innerText = `${name}:${content}`;
                 messageList.appendChild(li);
+                postMessageForm.querySelector('input[name=content').value="";
             }, function (error) {
                 alert('请改天留言');
             })
         }
     }
-    controller.init.call(controller, view);
+    controller.init.call(controller, view,model);
 
 }.call();
